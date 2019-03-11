2fbe52068b8e2a3663af592a6cbb0ff94d8c251d
safaa <31968900+Safaazaoui@users.noreply.github.com>
2019-03-10 22:35:46 -0400

    Update jeu3.sh

--- 1.KafkaCat/300108495/jeu3.sh
+++ 1.KafkaCat/300108495/jeu3.sh
@@ -4,7 +4,7 @@ function main {
    echo "Copy de fichier "
    for artist in ./artist*.json; do
     for ((i=1; i<=4 ;i++)); do
-        docker exec --interactive kafka kafka-console-producer --broker-list kafka:9092 --topic artist < ./artist$i.json
+        docker exec --interactive kafka kafka-console-producer --broker-list kafka:9092 --topic artists < ./artist$i.json
     done
 done
 }
