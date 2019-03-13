fe5357aead3723f1aae97a7f4a0b338a76b3b1d6
ameliedub <31968859+ameliedub@users.noreply.github.com>
2019-03-13 13:24:05 -0400

    Update jeu1.sh

--- 1.KafkaCat/300089781/jeu1.sh
+++ 1.KafkaCat/300089781/jeu1.sh
@@ -3,7 +3,7 @@
 function main {
    echo "Clients "
    for client in ./client*.json; do
-    for ((i=1; i<=2 ;i++)); do
+    for ((i=1; i<=3 ;i++)); do
         docker exec --interactive kafka kafka-console-producer --broker-list kafka:9092 --topic clients < ./client$i.json
     done
 done
