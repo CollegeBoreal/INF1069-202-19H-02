b77a22cf5bed697f7f23d96fd215851f78afd6c1
ameliedub <31968859+ameliedub@users.noreply.github.com>
2019-03-13 13:23:19 -0400

    Update jeu.sh

--- 1.KafkaCat/300089781/jeu.sh
+++ 1.KafkaCat/300089781/jeu.sh
@@ -4,7 +4,7 @@
 function main {
    echo "foodie"
    for repas in ./repas*.json; do
-   for ((i=1; i<=2; i++)); do
+   for ((i=1; i<=3; i++)); do
 	docker exec --interactive kafka kafka-console-producer --broker-list kafka:9092 --topic repas < ./repas$i.json
    done
 done
