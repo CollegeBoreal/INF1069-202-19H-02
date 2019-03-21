72b5f4d2f259e61b35889158f900f7010126a033
safaa <safaazaoui@gmail.com>
2019-03-21 13:50:36 -0400

    safaa

--- 1.KafkaCat/300108495/jeu1.sh
+++ 1.KafkaCat/300108495/jeu1.sh
@@ -4,9 +4,8 @@ function main {
    echo "Copy de fichier "
    for client in ./client*.json; do
         docker exec --interactive kafka kafka-console-producer --broker-list kafka:9092 --topic clients_info < ./client$.json  
-        
-    
-done
+
+   done
 }
 
 main
