15ebb6886a2fa3a86f1570b4541f4cbbf9db5bce
Bouchichi <bouchichi.mostafa@gmail.com>
2019-03-21 14:35:37 -0400

    Mostafa

---
+++ 1.KafkaCat/300107710/client.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function main {
+   echo "Copy de fichier"
+   for client in client*.json
+   do
+     docker exec --interactive kafka kafka-console-producer --broker-list kafka:9092 --topic client <  ./$client
+   done
+}
+
+main
+
