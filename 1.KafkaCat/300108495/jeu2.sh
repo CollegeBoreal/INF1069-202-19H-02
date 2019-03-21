0ec4d9dd5dd84fad86ef5495909d4efb67dc9fcd
safaa <31968900+Safaazaoui@users.noreply.github.com>
2019-03-21 13:55:35 -0400

    Update jeu2.sh

--- 1.KafkaCat/300108495/jeu2.sh
+++ 1.KafkaCat/300108495/jeu2.sh
@@ -2,9 +2,9 @@
 
 function main {
    echo "Copy de fichier "
-   for product in ./product*.json; do
-    
-        docker exec --interactive kafka kafka-console-producer --broker-list kafka:9092 --topic products < ./product$.json
+   for product in ./product*.json;
+   do  
+    docker exec --interactive kafka kafka-console-producer --broker-list kafka:9092 --topic client < ./$product
   
 done
 }
