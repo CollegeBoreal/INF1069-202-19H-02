9b573fa26eec8cd488725222e0d5e88231c8b733
Setra <setrar@valiha.com>
2019-03-21 11:50:30 -0400

    .

--- 1.KafkaCat/300098957/jeu.sh
+++ 1.KafkaCat/300098957/jeu.sh
@@ -2,9 +2,10 @@
 
 function main {
    echo "Copy de fichier"
-   docker exec --interactive kafka kafka-console-producer \
-               --broker-list kafka:9092 \
-               --topic my_topic < ./file1.json
+   for client in client*.json
+   do
+     docker exec --interactive kafka kafka-console-producer --broker-list kafka:9092 --topic repas <  ./$client
+   done
 }
 
 main
