9a69ab115df2d774dbcb172a14c7f4785216db62
setrar <setrar@valiha.com>
2019-03-27 14:51:36 -0400

    .

--- 1.KafkaCat/300105468/jeu1.sh
+++ 1.KafkaCat/300105468/jeu1.sh
@@ -2,9 +2,9 @@
 
 function main {
    echo "Copy de fichier "
-   for chanteur in ./chanteur*.json; do
-    for ((i=1; i<=4 ;i++)); do
-        docker exec --interactive kafka kafka-console-producer --broker-list kafka:9092 --topic chanteurs < ./chanteur$i.json
+   for chanteur in ./chanteur*.json
+   do
+      docker exec --interactive kafka kafka-console-producer --broker-list kafka:9092 --topic chanteurs < ./$chanteur
     done
 done
 }
