15b6afad4ec61230942632c02b915906d79f426f
chera <fotahinde@gmail.com>
2019-03-28 15:03:43 -0400

    Hamidou

--- 1.KafkaCat/300105468/jeu1.sh
+++ 1.KafkaCat/300105468/jeu1.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 function main {
-   echo "Copy de fichier "
-   for chanteur in ./chanteur*.json; do
-    for ((i=1; i<=4 ;i++)); do
-        docker exec --interactive kafka kafka-console-producer --broker-list kafka:9092 --topic reggae_music < ./chanteur$i.json
-    done
-done
+   echo "Copy de fichier"
+   for chanteur in chanteur*.json
+   do
+      docker exec --interactive kafka kafka-console-producer --broker-list kafka:9092 --topic chanteurs <  ./$chanteur
+  done 
 }
 
 main
+
+
+
+
+
+
+
+
+
+
