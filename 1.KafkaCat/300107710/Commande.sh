1d28e472f8d6786b66e76ec3a9503fc8865f7cfd
Bouchichi <bouchichi.mostafa@gmail.com>
2019-04-01 09:06:46 -0400

    Mostafe

--- 1.KafkaCat/300107710/Commande.sh
+++ 1.KafkaCat/300107710/Commande.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-
 function main {
-   echo 'Hello world'
-   for filename in ./commande*.json; do
-     for ((i=0; i<=6; i++)); do
-           docker exec --interactive kafka kafka-console-producer --broker-list kafka:9092 --topic commande < ./commande$i.json
-    done
-done
+   echo "Copy de fichier"
+   for commande in Commande*.json
+   do
+     docker exec --interactive kafka kafka-console-producer --broker-list kafka:9092 --topic commande <  ./$commande
+   done
 }
 
+
 main
+
+
 
