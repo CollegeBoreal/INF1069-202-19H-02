feba57da064d1f3ac8e8e6680334cc12002f3730
Thierno Hamidou Diallo <31969066+Hamidoud@users.noreply.github.com>
2019-04-03 14:54:25 -0400

    Update jeu2.sh

--- 1.KafkaCat/300105468/jeu2.sh
+++ 1.KafkaCat/300105468/jeu2.sh
@@ -4,13 +4,8 @@ function main {
    echo "Copy de fichier"
    for chanson in chanson*.json
    do
-<<<<<<< HEAD
-  docker exec --interactive kafka kafka-console-producer --broker-list kafka:9092 --topic chansons <  ./$chanson
-done 
-=======
      docker exec --interactive kafka kafka-console-producer --broker-list kafka:9092 --topic chansons <  ./$chanson
    done
->>>>>>> 0553fd6a611cd3d9663b4eeb25952e60d62f613e
 }
 
 main
